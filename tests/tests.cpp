5873155eeb06d16a5f2a9779594f83418e48a0c2
Amphaal <guillaume.vara@gmail.com>
2021-10-13 16:36:16 +0200

    ok

---
+++ tests/tests.cpp
@@ -0,0 +1,23 @@
+// StupidHTTPDownloader
+// Really stupid library to download HTTP(S) content
+// Copyright (C) 2021 Guillaume Vara
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#define CATCH_CONFIG_MAIN
+
+#include <StupidHTTPDownloader/Downloader.h>
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("", "[]") {
+
+}
